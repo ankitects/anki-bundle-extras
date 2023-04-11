66053d42ff89f1538d3361f76cc2561a02b731c4
Damien Elmes <gpg@ankiweb.net>
2023-04-11 17:50:35 +1000

    Switch to zstd for Qt bundle

--- mac/prepare-qt6.sh
+++ mac/prepare-qt6.sh
@@ -12,5 +12,5 @@ python3 prepare-qt.py $qtbase/$qtver $desktop arm64/dist/pyqt6
 arch -arch x86_64 python3 prepare-qt.py $qtbase/$qtver $desktop amd64/dist/pyqt6
 
 mkdir -p ../dist
-(cd arm64/dist/pyqt6 && tar czvf ../../../../dist/pyqt${qtvershort}-mac-arm64.tar.gz .)
-(cd amd64/dist/pyqt6 && tar czvf ../../../../dist/pyqt${qtvershort}-mac-amd64.tar.gz .)
+(cd arm64/dist/pyqt6 && tar -c --use-compress-program 'zstd --long -19 -T0' -vf ../../../../dist/pyqt${qtvershort}-mac-arm64.tar.zst .)
+(cd amd64/dist/pyqt6 && tar -c --use-compress-program 'zstd --long -19 -T0' -vf ../../../../dist/pyqt${qtvershort}-mac-amd64.tar.zst .)
